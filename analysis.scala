bb75a58a2a2fcdc51809708f99a42a288123aa3e
SrujanaN <srujana.n93@gmail.com>
2019-11-10 19:34:00 -0600

    Adding more queries to find correlation between best and worst business based on rating and review count.

--- analysis.scala
+++ analysis.scala
@@ -7,3 +7,20 @@ business.createOrReplaceTempView("business")
 
 val join_output = spark.sql("Select * from business order by review_count, stars desc")
 join_output.show(1000)
+
+// Output business based on review count and rating in descending order
+val join_output = spark.sql("Select * from business order by review_count, stars desc")
+join_output.show(1000)
+
+
+// Output best business based on review count >=10 and rating>=4.0 in descending order.
+val best_business = spark.sql("select * from business where review_count > 10 and stars>= 4.0 order by
+review_count,stars desc)
+
+best_business.show()
+
+// Output worst business based on review count >=10 and rating<=2.0 in descending order.
+val worst_business = spark.sql("select * from business where review_count > 10 and stars<= 2.0 order by
+review_count,stars desc)
+
+worst_business.show()
